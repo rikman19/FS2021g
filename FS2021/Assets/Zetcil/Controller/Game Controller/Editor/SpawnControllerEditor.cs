cfa526cf7d049da4a86f67340d0e33799462b11b
Rickman Roedavan <rikman@telkomuniversity.ac.id>
2021-01-19 11:15:41 +0700

    Asset, Controller

---
+++ FS2021/Assets/Zetcil/Controller/Game Controller/Editor/SpawnControllerEditor.cs
@@ -0,0 +1,123 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Zetcil
+{
+    [CustomEditor(typeof(SpawnController)), CanEditMultipleObjects]
+    public class SpawnControllerEditor : Editor
+    {
+
+        public SerializedProperty
+            isEnabled,
+            InvokeType,
+            PrefabList,
+            TargetPrefab,
+            CurrentIndex,
+            TargetPosition,
+            usingParent,
+            TargetParent,
+            AfterSpawn,
+            TotalSpawn,
+            MaxSpawn,
+            usingDelay,
+            Delay,
+            usingInterval,
+            Interval,
+            usingScale,
+            SpawnScale
+            ;
+
+        void OnEnable()
+        {
+            // Setup the SerializedProperties
+            isEnabled = serializedObject.FindProperty("isEnabled");
+            InvokeType = serializedObject.FindProperty("InvokeType");
+            PrefabList = serializedObject.FindProperty("PrefabList");
+            TargetPrefab = serializedObject.FindProperty("TargetPrefab");
+            CurrentIndex = serializedObject.FindProperty("CurrentIndex");
+            TargetPosition = serializedObject.FindProperty("TargetPosition");
+            usingParent = serializedObject.FindProperty("usingParent");
+            TargetParent = serializedObject.FindProperty("TargetParent");
+            usingDelay = serializedObject.FindProperty("usingDelay");
+            AfterSpawn = serializedObject.FindProperty("AfterSpawn");
+            TotalSpawn = serializedObject.FindProperty("TotalSpawn");
+            MaxSpawn = serializedObject.FindProperty("MaxSpawn");
+            Delay = serializedObject.FindProperty("Delay");
+            usingInterval = serializedObject.FindProperty("usingInterval");
+            Interval = serializedObject.FindProperty("Interval");
+            usingScale = serializedObject.FindProperty("usingScale");
+            SpawnScale = serializedObject.FindProperty("SpawnScale");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+
+            EditorGUILayout.PropertyField(isEnabled);
+
+            bool check = isEnabled.boolValue;
+
+            if (check)
+            {
+
+                EditorGUILayout.PropertyField(InvokeType);
+
+                EditorGUILayout.PropertyField(PrefabList);
+                EditorGUILayout.PropertyField(TargetPrefab);
+                if (TargetPrefab.arraySize == 0)
+                {
+                    EditorGUILayout.HelpBox("Required Field(s) Null / None", MessageType.Error);
+                }
+
+                EditorGUILayout.PropertyField(CurrentIndex);
+                EditorGUILayout.PropertyField(TotalSpawn);
+                EditorGUILayout.PropertyField(MaxSpawn);
+
+                EditorGUILayout.PropertyField(TargetPosition);
+                if (TargetPosition.objectReferenceValue == null)
+                {
+                    EditorGUILayout.HelpBox("Required Field(s) Null / None", MessageType.Error);
+                }
+
+                EditorGUILayout.PropertyField(usingParent);
+                if (usingParent.boolValue)
+                {
+                    EditorGUILayout.PropertyField(AfterSpawn);
+                    EditorGUILayout.PropertyField(TargetParent);
+                    if (TargetParent.objectReferenceValue == null)
+                    {
+                        EditorGUILayout.HelpBox("Required Field(s) Null / None", MessageType.Error);
+                    }
+                }
+
+                //--Invoke type (3)
+                if ((GlobalVariable.CInvokeType)InvokeType.enumValueIndex == GlobalVariable.CInvokeType.OnDelay)
+                { 
+                    EditorGUILayout.PropertyField(usingDelay, true);
+                    if (usingDelay.boolValue)
+                    {
+                        EditorGUILayout.PropertyField(Delay, true);
+                    }
+                }
+                if ((GlobalVariable.CInvokeType)InvokeType.enumValueIndex == GlobalVariable.CInvokeType.OnInterval)
+                {
+                    EditorGUILayout.PropertyField(usingInterval, true);
+                    if (usingInterval.boolValue)
+                    {
+                        EditorGUILayout.PropertyField(Interval, true);
+                    }
+                }
+
+                EditorGUILayout.PropertyField(usingScale);
+                EditorGUILayout.PropertyField(SpawnScale);
+            }
+            else
+            {
+                EditorGUILayout.HelpBox("Prefab Status: Disabled", MessageType.Error);
+            }
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+
+}
---
+++ FS2021/Assets/Zetcil/Controller/Game Controller/Scripts/FlipFlopController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using TechnomediaLabs;
+
+namespace Zetcil
+{
+    public class FlipFlopController : MonoBehaviour
+    {
+        [Space(10)]
+        public bool isEnabled;
+
+        [Header("Flip Flop Settings")]
+        public float Interval;
+
+        [Header("Flip Settings")]
+        public bool usingFlipEvent;
+        public UnityEvent FlipEvent;
+
+        [Header("Flop Settings")]
+        public bool usingFlopEvent;
+        public UnityEvent FlopEvent;
+
+        [Header("Status")]
+        [ReadOnly] public float currentClock = 0;
+
+        public void SetEnabled(bool aValue)
+        {
+            isEnabled = aValue;
+        }
+        // Start is called before the first frame update
+        void Start()
+        {
+            InvokeRepeating("ExecuteFlipFlop", 0, Interval);
+        }
+
+        public void ExecuteFlipFlop()
+        {
+            if (isEnabled)
+            {
+                currentClock++;
+                if (currentClock % 2 == 0)
+                {
+                    if (usingFlipEvent)
+                    {
+                        FlipEvent.Invoke();
+                    }
+                }
+                else
+                {
+                    if (usingFlopEvent)
+                    {
+                        FlopEvent.Invoke();
+                    }
+                }
+            }
+        }
+
+        public void TerminateFlipFlop()
+        {
+            CancelInvoke();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
